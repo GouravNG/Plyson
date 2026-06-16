d3a6e89ad9623274a269b8644ed2b48dadcbd196
Gourav <gouravgunaga555@gmail.com>
2026-06-16 22:08:51 +0530

    test: updated examples to  global setup and teardown

---
+++ examples/marketplace/suites/plyson.teardown.ts
@@ -0,0 +1,8 @@
+import { test as teardown, expect } from '@playwright/test';
+import { bootstrapTeardown } from '@plyson/test';
+
+/**
+ * This is the entry point for plyson global teardown.
+ * It runs project.json afterAll hooks.
+ */
+await bootstrapTeardown(teardown, expect);
